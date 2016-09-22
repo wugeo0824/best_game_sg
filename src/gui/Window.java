fb58ae7a0362871bdd01e79f5e7d31c220d1b326
nicoleZY <e0013456@u.nus.edu>
2016-09-23 02:33:23 +0800

    update GUI

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -6,6 +6,8 @@ import java.awt.GridLayout;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -15,6 +17,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import game.GameNodeImpl;
 import message.PlayerAction;
@@ -25,20 +28,29 @@ import model.Treasure;
 
 public class Window extends JFrame {
 	private static final long serialVersionUID = 1509090869849539876L;
-	
+
 	public int N; // the maze is an N-by-N grid
 	public int K; // the number of treasures
 
 	private String title; // showed in the top of the window
-	private String username; // part of the title TODO change into exactly two characters
+	private String username; // part of the title TODO change into exactly two
+								// characters
 	private String server; // part of the title, identify primary/backup server
-	private String status = "[waiting]"; // part of the title, waiting is the default value of game status
+	private String status = "[waiting]"; // part of the title, waiting is the
+											// default value of game status
 
-	private Object[][] playerData; 
-	private Player[] playerArray;
-	private Treasure[] treasureArray;
+	private Object[][] table;
+	private ArrayList<Player> playerList;
+	private ArrayList<Treasure> treasureList;
 	private String[][] btnName;
 	private GameNodeImpl localGame;
+	private DefaultTableModel tableModel;
+	private static String tableTitle[] = { "Player", "Score" };
+	private JPanel p1;
+	private JPanel p2;
+	private PlayerAction action = null;
+	private int key;
+	private MyKeyListener myKeylistener;
 
 	public Window(GameNodeImpl localGame) throws RemoteException {
 		Maze maze = localGame.getMaze();
@@ -53,72 +65,61 @@ public class Window extends JFrame {
 
 		// Initialize the variables
 		btnName = new String[N][N];
-		
-		Set<Map.Entry<String, Player>> playerInfoSet = maze.getPlayers().entrySet();
-		playerArray = new Player[playerInfoSet.size()];
-		int i = 0;
-		Iterator<Map.Entry<String, Player>> pl = playerInfoSet.iterator();
-		while (pl.hasNext()){
-			playerArray[i] = pl.next().getValue();
-			i++;
-		}
-		
-		Set<Map.Entry<String, Treasure>> treasureSet = maze.getTreasures().entrySet();
-		treasureArray = new Treasure[treasureSet.size()];
-		i = 0;
-		Iterator<Map.Entry<String, Treasure>> tr = treasureSet.iterator();
-		while (tr.hasNext()){
-			treasureArray[i] = tr.next().getValue();
-			i++;
-		}
-		
-		playerData = new Object[playerArray.length][2];
-		
-		// Create panel p1 for the scores at the left part of the window
-		JPanel p1 = new JPanel();
-		String[] columnNames = { "Player", "Score" }; // title of the table
-		
-		getAllPlayerData();		
-		JTable playerTable = new JTable(playerData, columnNames);
-		JScrollPane scrollPane = new JScrollPane(playerTable);
-		scrollPane.setPreferredSize(new Dimension(150, 700));
-		p1.add(scrollPane);
 
-		// Create panel p2 for the positions at the right of the window
-		JPanel p2 = new JPanel();
-		p2.setLayout(new GridLayout(N, N));
-		
-		getAllPositions();
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				JButton btn = new JButton();
-				btn.setText(btnName[r][c]);
-				btn.setPreferredSize(new Dimension(50, 50));
-				btn.setBorder(BorderFactory.createEtchedBorder());
-				p2.add(btn);
-			}
-		}
+		playerList = getPlayerList(maze.getPlayers());
+		treasureList = getTreasureList(maze.getTreasures());
+
+		setTableData();
+		setAllPositions();
 
 		// add the two panels to the frame
 		add(p1, BorderLayout.WEST);
 		add(p2, BorderLayout.EAST);
-		
-		// add key listener
-		setFocusable(true);
-		addKeyListener(new MyKeyListener ());
+
+		addKeyListener();
 		
 		// update title
 		setWTitle();
 	}
 
+	private ArrayList<Player> getPlayerList(HashMap<String, Player> players) {
+		Set<Map.Entry<String, Player>> playerInfoSet = players.entrySet();
+		playerList = new ArrayList<Player>();
+		Iterator<Map.Entry<String, Player>> pl = playerInfoSet.iterator();
+		while (pl.hasNext()) {
+			playerList.add(pl.next().getValue());
+		}
+		return playerList;
+	}
+
+	private ArrayList<Treasure> getTreasureList(HashMap<String, Treasure> treasures) {
+		Set<Map.Entry<String, Treasure>> treasureSet = treasures.entrySet();
+		treasureList = new ArrayList<Treasure>();
+		Iterator<Map.Entry<String, Treasure>> tr = treasureSet.iterator();
+		while (tr.hasNext()) {
+			treasureList.add(tr.next().getValue());
+		}
+		return treasureList;
+	}
+	
+	private void addKeyListener() {
+		if (playerList.size() > 1) {
+			this.setFocusable(true);
+			myKeylistener = new MyKeyListener();
+			this.addKeyListener(myKeylistener);
+			} else {
+				this.removeKeyListener(myKeylistener);
+			}
+	}
 
 	/**
 	 * WINDOW TITLE PART
 	 */
-//	private void getUsername() {
-//		username = JOptionPane.showInputDialog(null, "Welcome!\n" + "Please setup an username first.", "Maze",
-//				JOptionPane.PLAIN_MESSAGE);
-//	}
+	// private void getUsername() {
+	// username = JOptionPane.showInputDialog(null, "Welcome!\n" + "Please setup
+	// an username first.", "Maze",
+	// JOptionPane.PLAIN_MESSAGE);
+	// }
 
 	private void getServer() {
 		if (localGame.isPrimary()) {
@@ -132,52 +133,103 @@ public class Window extends JFrame {
 	}
 
 	private void getStatus() {
-		if (playerArray.length > 1) {
+		if (playerList.size() > 1) {
 			status = "[started]";
 		}
 	}
 
 	public String getWTitle() {
-		//getUsername();
+		// getUsername();
 		getStatus();
 		getServer();
 
 		title = username + server + status;
 		return title;
 	}
-	
-	public void setWTitle(){
+
+	public void setWTitle() {
 		this.setTitle(getWTitle());
 	}
-	
 
 	/**
 	 * WINDOW LEFT PART
 	 */
-	public void getAllPlayerData() {
+	public Object[][] getTableData() {
+		table = new Object[playerList.size()][2];
 		// set players id and score in JTable
-		for (int k = 0; k < playerArray.length; k++) {
-			playerData[k][0] = playerArray[k].getName();
-			playerData[k][1] = playerArray[k].getCurrentScore();
+		for (int k = 0; k < playerList.size(); k++) {
+			table[k][0] = playerList.get(k).getName();
+			table[k][1] = playerList.get(k).getCurrentScore();
 		}
+		return table;
 	}
 
+	public JPanel setTableData() {
+		// Create panel p1 for the scores at the left part of the window
+		p1 = new JPanel();
+		table = getTableData();
+		tableModel = new DefaultTableModel(table, tableTitle);
+		JTable playerTable = new JTable(tableModel);
+		JScrollPane scrollPane = new JScrollPane(playerTable);
+		scrollPane.setPreferredSize(new Dimension(150, 700));
+		p1.add(scrollPane);
+		return p1;
+
+	}
+
+	public void updateTableData() {
+		p1.removeAll();
+		this.remove(p1);
+		this.add(setTableData(), BorderLayout.WEST);
+	}
 
 	/**
 	 * WINDOW RIGHT PART
+	 * 
+	 * @return
 	 */
-	public void getAllPositions(){
-		// set players positions as players names
-		for (int k = 0; k < playerArray.length; k++) {
-			Location locP = playerArray[k].getCurrentLocation();
-			btnName[locP.getLocationX()][locP.getLocationY()] = playerArray[k].getName();
+	public String[][] getAllPositions() {
+		for (int m = 0; m < N; m++) {
+			for (int n = 0; n < N; n++) {
+				btnName[m][n] = "";
+			}
 		}
 		
-		// set treasures positions as "*"	
-		for (int k = 0; k < treasureArray.length; k++) {
-			Location locT = treasureArray[k].getLocation();
+		// set players positions as players names
+		for (int k = 0; k < playerList.size(); k++) {
+			Location locP = playerList.get(k).getCurrentLocation();
+			btnName[locP.getLocationX()][locP.getLocationY()] = playerList.get(k).getName();
+		}
+
+		// set treasures positions as "*"
+		for (int k = 0; k < treasureList.size(); k++) {
+			Location locT = treasureList.get(k).getLocation();
 			btnName[locT.getLocationX()][locT.getLocationY()] = "*";
 		}
+		return btnName;
+	}
+
+	public JPanel setAllPositions() {
+		// Create panel p2 for the positions at the right of the window
+		p2 = new JPanel();
+		p2.setLayout(new GridLayout(N, N));
+		btnName = getAllPositions();
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				JButton btn = new JButton();
+				btn.setText(btnName[r][c]);
+				btn.setPreferredSize(new Dimension(50, 50));
+				btn.setBorder(BorderFactory.createEtchedBorder());
+				p2.add(btn);
+			}
+		}
+		return p2;
+	}
+
+	public void updateAllPositions() {
+		p2.removeAll();
+		this.remove(p2);
+		this.add(setAllPositions(), BorderLayout.EAST);
 	}
 
 	/**
@@ -185,46 +237,71 @@ public class Window extends JFrame {
 	 */
 	class MyKeyListener extends KeyAdapter {
 		public void keyPressed(KeyEvent e) {
-			int key = e.getKeyCode();
-			PlayerAction action = null;
-			if (key == KeyEvent.VK_1){
+			key = e.getKeyCode();
+			if (key == KeyEvent.VK_1) {
 				action = PlayerAction.MOVE_LEFT;
-			} else if (key == KeyEvent.VK_2){
+			} else if (key == KeyEvent.VK_2) {
 				action = PlayerAction.MOVE_DOWN;
-			} else if (key == KeyEvent.VK_3){
+			} else if (key == KeyEvent.VK_3) {
 				action = PlayerAction.MOVE_RIGHT;
-			} else if (key == KeyEvent.VK_4){
+			} else if (key == KeyEvent.VK_4) {
 				action = PlayerAction.MOVE_UP;
-			} else if (key == KeyEvent.VK_9){
+			} else if (key == KeyEvent.VK_9) {
 				action = PlayerAction.QUIT;
-			} else if (key == KeyEvent.VK_0){
+			} else if (key == KeyEvent.VK_0) {
 				action = PlayerAction.STAY;
 			}
 			processKeyInput(action);
-			return;			
-		}
-	}
-	
-	private void processKeyInput(PlayerAction action) {
-		if (action == null){
 			return;
 		}
-		
-		if (action == PlayerAction.QUIT){
+	}
+
+	private void processKeyInput(PlayerAction action) {
+		if (action == null) {
+			return;
+		}
+
+		if (action == PlayerAction.QUIT) {
 			localGame.closeGame();
 			return;
 		}
-			
+
 		localGame.playerMadeAMove(action);
-	}
-	
-	public void updateGame(Maze newMaze){
-		//TODO update the GUI according to the maze
-	}
-	
-	public void close(){
-		//TODO when user request to quit the game
+		action = null;
+//		try {
+//			updateGame(localGame.getMaze());
+//		} catch (RemoteException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
+	public void update(Maze newMaze) {
+		playerList = getPlayerList(newMaze.getPlayers());
+		treasureList = getTreasureList(newMaze.getTreasures());
+
+		updateTableData();
+		updateAllPositions();
+		revalidate();
+		addKeyListener();
+		setWTitle();
+	}
+	public void updateGame(Maze newnewMaze) {
+		// update the GUI according to the maze
+
+		update(newnewMaze);
+
+		if (key == KeyEvent.VK_1 || key == KeyEvent.VK_2 || key == KeyEvent.VK_3 || key == KeyEvent.VK_4
+				|| key == KeyEvent.VK_9 || key == KeyEvent.VK_0) {
+			update(newnewMaze);
+			
+
+		}
+	}
+
+	public void close() {
+		// when user request to quit the game
+		super.dispose();
+	}
 
 }
